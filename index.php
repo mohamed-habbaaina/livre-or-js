66915625470fb3edab82358b105c67506de99709
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-02-20 11:52:04 +0100

    la page home

---
+++ index.php
@@ -0,0 +1,33 @@
+<!-- MOHAMED HABBAAINA Le 05/12/2022
+
+-->
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <script src="./app.js" defer></script>
+    <title>Home</title>
+</head>
+<body>
+<?php include 'includes/header.php' ?>
+<main>
+    <div class="hom">
+        <div class="hom_bnv">
+            <h1></h1>
+            <p>Chaque année début juillet,<b> le FIDMarseille, Festival International de Cinéma de Marseille</b> propose un programme de 130 films à près de 23 500 spectateurs, dans des cinémas, théâtres, bibliothèques, galeries d’art, amphithéâtres en plein air, à Marseille. Le festival présente un grand nombre de films en première mondiale, de premiers films, et s’impose aujourd’hui comme un gisement de nouvelles cinématographies, productions documentaires aussi bien que fictions.
+Pendant le festival sont également organisés le FIDLab, plateforme de soutien à la coproduction internationale, le FIDCampus, résidence de formation d’étudiants internationaux.</p>
+            <p>Nous apprécierions vraiment si vous pouviez prendre quelques minutes de votre temps pour laisser un commentaire sur votre expérience lors du festival dans notre <a href="./inscription.php"><span id="spanHome">Livre d'Or</span></a>. Vos commentaires peuvent aider à informer les futurs participants et à améliorer l'expérience globale du festival. Que vous ayez aimé les projections de films, les discussions en ligne, ou les rencontres avec les autres participants, vos commentaires sont importants pour nous.</p>
+        </div>
+    </div>
+
+</main>
+
+<?php include 'includes/footer.php' ?>
+</body>
+</html>
